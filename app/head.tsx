62adac9be853691d35aa781820da446cee88c1ae
Norbert Merkli <norbertmerkli11@gmail.com>
2022-11-07 11:02:15 +0100

    Change the project name

--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,15 @@
 export default function Head() {
     return (
         <>
-            <title>AskMate</title>
+            <title>Stack Overflow Clone</title>
             <meta
                 content="width=device-width, initial-scale=1"
                 name="viewport"
             />
-            <meta name="description" content="Ask your mate!" />
+            <meta
+                name="description"
+                content="A simple StackOverflow clone made for learning purposes."
+            />
             <link rel="icon" href="/favicon.ico" />
         </>
     );
