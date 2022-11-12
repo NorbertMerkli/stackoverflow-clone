dfb2961ff564d10388fed391f17f752380c6436d
Norbert Merkli <norbertmerkli11@gmail.com>
2022-11-12 16:49:00 +0100

    Make the app logo component a link to the index page

--- components/app-logo.tsx
+++ components/app-logo.tsx
@@ -1,13 +1,14 @@
+import Link from "next/link";
 import { Stackoverflow } from "./vectors/logos/stackoverflow";
 
 export function AppLogo() {
     return (
-        <div className="flex flex-row justify-center items-center">
+        <Link href="/" className="flex flex-row justify-center items-center">
             <Stackoverflow />
             <h1 className="text-3xl font-logo text-dark ml-2">
                 stack<span className="font-bold">overflow</span>
                 <span className="text-grey italic">clone</span>
             </h1>
-        </div>
+        </Link>
     );
 }
