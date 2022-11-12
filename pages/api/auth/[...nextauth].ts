b0b93ff1ddcc0f7ad93c64dbf70605adbd2ed2c3
Norbert Merkli <norbertmerkli11@gmail.com>
2022-11-12 17:35:59 +0100

    Add 'CredentialsProvider' for user registration

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,41 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
-import { createUser } from "@lib/query";
+import { createUser, getUserByEmail } from "@lib/query";
 
 export const authOptions: NextAuthOptions = {
     providers: [
+        CredentialsProvider({
+            // According to the official documentation,
+            // the content of the 'credentials' field is mainly used to generate a form on the default sign-in page.
+            // Currently, I'm using a self-made sign-in page so these values are irrelevant in my case.
+            // That's why they are empty.
+            credentials: {
+                email: {},
+                password: {},
+            },
+            async authorize(credentials) {
+                if (credentials) {
+                    const user = await getUserByEmail(credentials.email);
+
+                    if (
+                        user &&
+                        user.password &&
+                        (await bcrypt.compare(
+                            credentials.password,
+                            user.password
+                        ))
+                    ) {
+                        return user;
+                    }
+                }
+
+                return null;
+            },
+        }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
