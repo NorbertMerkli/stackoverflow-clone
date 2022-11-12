b80098dd9a5de646e0d9ba822ab8665a50840b57
Norbert Merkli <norbertmerkli11@gmail.com>
2022-11-12 11:23:51 +0100

    Fix FALSE reference error in the SigninWithProviderPanel component

--- components/signin-with-provider.tsx
+++ components/signin-with-provider.tsx
@@ -1,18 +1,30 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { useEffect, useRef } from "react";
 import { GitHubLogo } from "./vectors/logos/github";
 import { GoogleLogo } from "./vectors/logos/google";
 
 export function SigninWithProviderPanel() {
-    const callbackUrl = document.referrer.includes(document.location.origin)
-        ? document.referrer
-        : document.location.origin;
+    // Despite my use of the "use client" directive,
+    // the framework shows a ReferenceError caused by the 'document' object unless I wrap it in a 'useEffect' hook.
+    //
+    // Complaint: If the file is marked with the "use client" directive,
+    // there should be no errors or warnings caused by the usage of browser APIs.
+    const callbackUrl = useRef("");
+
+    useEffect(() => {
+        callbackUrl.current = document.referrer.includes(
+            document.location.origin
+        )
+            ? document.referrer
+            : document.location.origin;
+    }, []);
 
     return (
         <section className="grid grid-cols-2 grid-rows-1 gap-4">
-            <SigninWithGoogle callbackUrl={callbackUrl} />
-            <SigninWithGitHub callbackUrl={callbackUrl} />
+            <SigninWithGoogle callbackUrl={callbackUrl.current} />
+            <SigninWithGitHub callbackUrl={callbackUrl.current} />
         </section>
     );
 }
