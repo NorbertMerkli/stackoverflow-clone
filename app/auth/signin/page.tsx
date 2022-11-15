f1d4192e3037c3eb1dcc116f7d05a2e10e850805
Norbert Merkli <norbertmerkli11@gmail.com>
2022-11-15 15:06:59 +0100

    Move referrer logic into the components that actually use it

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { headers } from "next/headers";
 
 import { AppLogo } from "@components/app-logo";
 import { Underline } from "@components/vectors/underline";
@@ -7,8 +6,6 @@ import { SigninWithProviderPanel } from "@components/signin-with-provider";
 import { SigninWithCredentialsForm } from "@components/signin-form";
 
 export default function Signin() {
-    const referrer = headers().get("referer") || "";
-
     return (
         <div className="center h-screen">
             <main className="sm:shape sm:shadow-lg flex flex-col justify-center w-full sm:w-auto h-full sm:h-auto sm:py-16 px-8 xl:px-16 bg-white">
@@ -16,18 +13,18 @@ export default function Signin() {
                 <p className="px-8 my-8 text-center text-dark">
                     Enter your details to get sign in to your account
                 </p>
-                <SigninWithCredentialsForm referrer={referrer} />
+                <SigninWithCredentialsForm />
                 <div className="center my-8">
                     <div className="divider-h w-8 mr-4"></div>
                     <p className="text-dark">or continue with</p>
                     <div className="divider-h w-8 ml-4"></div>
                 </div>
-                <SigninWithProviderPanel referrer={referrer} />
+                <SigninWithProviderPanel />
                 <p className="mt-8 text-center text-grey">
                     Don&apos;t have an account?
                     <Link
                         className="group relative ml-2 font-bold text-dark"
-                        href=""
+                        href="/auth/signup"
                     >
                         Sign up for free
                         <Underline />
