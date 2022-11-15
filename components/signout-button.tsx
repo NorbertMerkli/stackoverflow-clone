79c149bc709c863169679af0870405d97e751367
Norbert Merkli <norbertmerkli11@gmail.com>
2022-11-15 15:15:44 +0100

    Simplify the signout button component

--- components/signout-button.tsx
+++ components/signout-button.tsx
@@ -4,10 +4,6 @@ import { signOut } from "next-auth/react";
 
 export function SignoutButton() {
     return (
-        <button
-            onClick={() => signOut({ callbackUrl: document.location.pathname })}
-        >
-            SignOut
-        </button>
+        <button onClick={() => signOut({ callbackUrl: "/" })}>SignOut</button>
     );
 }
