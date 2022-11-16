43c8296715351b2e6edaca6b085a492c7b5f017a
Norbert Merkli <norbertmerkli11@gmail.com>
2022-11-16 10:23:21 +0100

    Make the app logo responsive

--- components/app-logo.tsx
+++ components/app-logo.tsx
@@ -3,9 +3,12 @@ import { Stackoverflow } from "./vectors/logos/stackoverflow";
 
 export function AppLogo() {
     return (
-        <Link href="/" className="flex flex-row justify-center items-center">
+        <Link
+            href="/"
+            className="flex flex-col min-[370px]:flex-row justify-center items-center"
+        >
             <Stackoverflow />
-            <h1 className="text-3xl font-logo text-dark ml-2">
+            <h1 className="mt-4 min-[370px]:mt-0 text-3xl font-logo text-dark ml-2">
                 stack<span className="font-bold">overflow</span>
                 <span className="text-grey italic">clone</span>
             </h1>
